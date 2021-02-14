de02005e81a8632c8354c8d9814681473fb7d17c
thamaraAicodeTech <thamara@aicodetech.com>
2021-02-14 23:34:15 +0530

    app init

--- lib/const.dart
+++ lib/const.dart
@@ -12,9 +12,9 @@ class AppData{
   static const String LOGOPATH = 'assets/images/logo.png';
   static const String BACKGROUNDPATH = 'assets/images/background.jpg';
   static Color secondaryColor= Color(0xff393939);
-  static Color blackColor= Color(0xff393939);
   static Color primaryColor= Color(0xFFFFFBFB);
   static Color whiteColor= Color(0xFFFFFBFB);
+  static Color blackColor= Color(0xff393939);
   static Color thirdColor = Color(0xFFFF7D05);
   static int reviewCount = 0;
   static int clapCount = 0;
